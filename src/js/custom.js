b728d723b098b8bf627e717b32227ed8d33c88bc
Freddy Piña <Freddy Pia>
2017-01-08 21:21:23 -0300

    agregado kilometros

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 	$("#btn-ruta").on('click', function(){
 		$("#transport").removeClass('hidden');
+		console.log("click");
 	})
 	$('.check').change(function() {
 		if ($('.check:checked').length) {
@@ -17,5 +18,14 @@ $(document).ready(function(){
             $("#btn-ruta").removeAttr("disabled")
         }
     });	
+	$(".getconsumo").on('click', function(event){
+		 //alert("aqui");
+		 //alert(event.target.id);
+		 if (event.target.id == "auto") {
+		   //$('#distance_value').val();
+           $('#valorop2').html($('#distance_value').val());		   
+		 }
+		
+	})
 });
 
